fe2bd97854721caaae4198b854e835347e2fc12d
lyju777 <lyju777@hanmail.net>
2024-09-24 22:16:45 +0900

    Rendering Elements

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+setInterval(() => {
 root.render(
   <React.StrictMode>
-    <Library />
+    <Clock />
   </React.StrictMode>
 );
+},1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
